a91f393f48fb2693cc7ec72d2e5b71d4cc9bfa76
Sandro <34207865+SandroSartoni@users.noreply.github.com>
2018-01-20 12:20:25 +0100

    Update football.s

--- ARM_Assembly_Code/32_bits/football.s
+++ ARM_Assembly_Code/32_bits/football.s
@@ -1,6 +1,101 @@
-	.data
+/* Solution provided by Sandro Sartoni 
 
+**********************************************************************************************************************************
+
+Exam's text:
+
+A University soccer tournament accounts 9 teams grouped in 3 sets (A, B, C), each one hosting 3 teams, named A1, A2, A3 for
+set A, B1, B2, B3 for set B, and C1, C2, C3 for set C. During the first round, each team same plays two games against the other
+two in the set and a victory is awarded 3 points, a draw with goals is awarded 2 points to each team, a draw without goals
+is awarded 1 point to each team and a loss 0 points. After the first round the top teams of each set are admitted to the second
+round, plus the “best” team of the second classified, i.e. the second classified with more points. These four teams play one
+versus the other this way: Winner of set A vs. Winner of set B and Winner of set C vs. the best second classified. The winners
+of the two games (no tie is possible) play the final and the winner of the final is the winner of the University championship.
+
+It is requested to write an 8086 assembly program to manage the tournament games and rules, based on the scores that are
+received in input by the user. In particular, for each group, the program should present the user with the names of the two
+teams and the user has to enter the score (assuming that no team can score more than 9 goals in a single match). The program
+should compute and present the standings for each group at the end of the first round, together with the names of the teams
+which are admitted to the second round. Then, the program has to present the second round games, receive the results and
+identify the two teams, which will play the final. Lastly, the programs should present the two names of these two teams, playing
+the final and, based on the result received in input, present the winner of the tournament. For example:
+
+A1-A2 = 0-0; A1-A3 = 0-1; A3-A2 = 1-1; leads to the standings: A3 = 5 points; A2 = 3 points; A1 = 1 point
+B1-B2 = 2-0; B1-B3 = 2-1; B3-B2 = 1-1; leads to the standings: B1 = 6 points; B2 = 2 points; B3 = 2 points
+C1-C2 = 4-0; C1-C3 = 4-0; C3-C2 = 0-0; leads to the standings: C1 = 6 points; C2 = 1 point; C3 = 1 point
+
+The winners are, A3, B1, and C1, while the second best classified is A2. Therefore, second round games are: A3-B1, and C1-A2.
+Assume that A3-B1 = 0-1, and C1-A2 = 3-4, then the final will be B1 versus A2; assuming that B1-A2 = 4-5, then the winner of the
+tournament will be team A3.
+
+In the previous example, the identification of the teams admitted to second round is very easy as there have been no ties
+in the number of points awarded. It is ensured that no three-teams ties will occur. The rules in case of a two-teams tie are as
+follows (TIE BREAKER RULES):
+1.	The tie is won by the team with the lowest alphabetical letter;
+2.	In case of further tie, the tie is won by the team with the lowest subscript.
+
+For example: with the previous score, for group B, both B2 and B3 have 2 points and so the question is: which is the second
+classified in the set? According to rule #2, B2 is the second classified because it has a lower subscript (rule #1 has no
+effect here). For group C, both C2 and C3 have 1 point and according rule #2 the second classified is C2.
+
+Please observe that rule #2 can solve a tie inside a group, while rule #1 solves the ties across groups. For example, assuming
+that the three second classified are A3=2 points, B2=3 points, C2=3 points, then, out of B2 and C2, rule #1 decides the
+tie, in favor of B2.
+
+Tasks to be implemented and corresponding point (only fully completed items will be considered to award points)
+•	Item 1 (MANDATORY): write a running program fully implementing the first round ONLY and computing the standings after the
+			        first round. In case of two teams with the same number of points inside a group, any ranking of the two
+			        is acceptable. POINTS  22
+•	Item 2: in addition to Item 1, write a running program implementing the TIE BREAKER RULE #2 inside each group to determine
+		    each group standings; POINTS  +3
+•	Item 3: in addition to Item 2 (and 1), write a running program identifying the best second classified to be admitted to the
+		    second round, according to the following criteria: largest number of points, and, in case of a tie between two
+		    teams, TIE BREAKER RULE #1. POINTS  +4
+•	Item 4: in addition to Item 3 (and 2), write a running program implementing the second and third (i.e. final) rounds, and
+		    identifying the winning team of the tournament, POINTS  +4
+•	Bonus Item: in addition to Item 1, write a running program identifying ALL the teams (which could also be more than one) which
+		        have score the largest number of goals at the end of first round. POINTS  +4 (identifying only one of the many
+		        awards at most 2 extra points).
+Please consider that a maximum of 33 points can be accounted here; larger values will be “cut off” to 33.
+
+HINTS (observe that)
+•	It is ensured that no three-teams ties will occur 
+•	It is advised to design the program as a collection of modules, each one implementing the different Items.
+
+REQUIREMENTS (SHARP)
+•	It is not required to provide the optimal (shortest, most efficient, fastest) solution, but a working and clear one. 
+•	It is required to write at class time a short and clear explanation of the algorithm used.
+•	It is required to write at class time significant comments to the instructions.
+•	Input-output is not necessary in class-developed solution, but its implementation is mandatory for the oral exam.
+•	Minimum score to “pass” this part is 15 (to be averaged with second part and to yield a value at least 18)
+
+REQUIREMENTS ON THE I/O PART TO BE DONE AT HOME
+•	The databases (if any, i.e. not necessary in case) have to be defined and initialized inside the code 
+•	All inputs and outputs should be in readable ASCII form (no binary is permitted).
+
+*********************************************************************************************************************************       
+
+How to get this file running on Raspberry Pi 3:
+
+
+*/
+
+/* .data is a directive that allows us to define all the memory variables required in our program */
+
+	.data
+	
+/* .balign <power_of_two> aligns the address of whatever is below to <power_of_two> bytes. If the address is
+ * already a multiple of <power_of_two> bytes, it does nothing. This instruction is required by the processor in order to work
+ * correctly, failures to do so may generate errors */
+ 
 .balign 4
+
+/* To create memory variables, the syntax is: <variable_name>: .<type_of_variable> value1[,value2,...]. In the first line
+ * for example, we're generating a set of variables, each one that has 1 byte of size, named group1, with values
+ * 0xA1, 0xA2 and 0xA3 (hexadecimal). Below, .asciz "<content_of_the_string>" is creating a memory variable that is a
+ * string, in ascii format followed by a 0 byte (the 'z' in asciz), initialized with the content inside the quotation
+ * marks. */
+ 
 group1: .byte 0xA1,0xA2,0xA3
 group2: .byte 0xB1,0xB2,0xB3
 group3: .byte 0xC1,0xC2,0xC3
@@ -30,20 +125,38 @@ semi2: .byte 0
 res1: .byte 0
 res2: .byte 0
 
-
+/* .text is a directive that tells the assembler where the code starts */
 
 	.text
+
+/* .global printf/scanf are used in order to use printf and scanf in the assembly code. printf takes as arguments r0, that
+ * should store the content that has to be printed, and eventually r1, r2, r3 as other arguments if needed, i.e. to print
+ * "Hello World!" is sufficient to load on r0 the address of the string and nothing more, while to execute printf("The result
+ * of the sum is: %d",var); r0 has to store the address of the string, r1 has to store the address of var.
+ * scanf("%d",&var); instead needs to have on r0 the format of the data that we're going to insert, while r1 has to have the
+ * address of the memory variable in which we want to store the data. */
 
 	.global printf
 	.global scanf
 
-/* Function to handle each group's matches */
+/* Function to handle each group's matches, named insert_results */
 insert_results:
+
+/* stmfd is a function that stores multiple registers, in this case it's used to push multiple registers into the stack.
+ * In particular, sp! automatically updates the stack pointer (in 32-bit ARM assembly, the stack has to be aligned on a
+ * 4 bytes boundary, and it grows descending the memory, that means that every time something is pushed the address is
+ * decremented by 4). In the first instruction, we're storing all the registers from r3 to r6 (both included) and the link
+ * register, the link register is needed at the end of the procedure in order to return to the correct point in the main
+ * function. */
+ 
 stmfd sp!,{r3-r6,lr}
 stmfd sp!,{r0}
 
 /* First vs second  */
 stmfd sp!,{r0-r3}
+
+/* ldr r0,=firstrnd loads the address of the memory variable named firstrnd   */
+
 ldr r0,=firstrnd
 bl printf
 ldmfd sp!,{r0-r3}
