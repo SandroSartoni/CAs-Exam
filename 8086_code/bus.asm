e0620db583bf97f208b6dc511e6c8dbb9e3729c5
Sandro <34207865+SandroSartoni@users.noreply.github.com>
2018-01-25 11:57:30 +0100

    Update bus.asm

--- 8086_code/bus.asm
+++ 8086_code/bus.asm
@@ -1,3 +1,95 @@
+; Solution provided by Briand Wamba Foukmeniok
+;
+;**********************************************************************************************************************************
+;
+;Exam's text:
+;
+;Mr Omp uses the public transportation to reach his office at TP®. Basically he has 2 choices; first: take bus A, step off and then take bus B; second: take bus C, step off and take bus D. Mr Omp knows:
+;•	the departure schedule of buses A and C referred to the bus stop in front of his house (two arrays called, A_SCHED, and B_SCHED, respectively), and the departure schedule of B and D in correspondence of the swap-points A-to-B and C-to-D respectively (two arrays called, C_SCHED, and D_SCHED, respectively).
+;
+;He also knows how long does it take: 
+;•	for bus A to reach the swap-point A-to-B and for C to reach C-to-D (variable names: A_TO_B and C_TO_D);
+;•	for busses B and D to reach the building of TP® (variable names: B_TO_TP and D_TO_TP, respectively).
+;
+;It is requested to write a 8086 assembly program providing Mr. Omp with the fastest travel solution from his house to his office, depending on the time he exits his house (variable: H_LEAVE). Please consider also that:  
+;•	The duration of the full trip from the house to the office is less than 24 hours;
+;•	Each time is stored on 16 bits in the format: 000hhhhh 00mmmmmm, where the 5 h-bits represent the hour in 24-hours format (0-23), and the 6 m-bits the minutes (0-59);
+;•	The arrays A_SCHED, B_SCHED, C_SCHED and D_SCHED are sorted in increasing order (of time), have N_A, N_B, N_C and N_D elements (between 4 and 20), are known in advance and are the same all year long.
+;•	The last ride of the day for each bus line is followed by the first ride of the next day.
+;
+;Only fully completed items will be considered; given in input H_LEAVE, please solve only one among 1, 2, 3 and 4.
+;
+;Item 1: POINTS  21. Refer to the first choice only (take bus A, step off and then take bus B) and, ASSUMING that there exists a valid solution in the same day (i.e. departure and arrival occurring on the same day), compute & print:
+;1.	Arrival time to the swap-point;
+;2.	Departure time from the swap-point; 
+;3.	Arrival time to the office.
+;
+;Item 2: POINTS  25. Refer to the first choice only, with the CONSTRAINT that the travel should occur in the same day (i.e. departure and arrival taking place on the same day), compute & print:
+;1.	Name of solution (first or NO valid solution exists in the same day);
+;2.	Arrival time to the swap-point (if a valid solution exists);
+;3.	Departure time from the swap-point (if a valid solution exists);
+;4.	Arrival time to the office (if a valid solution exists).
+;Please observe that there could be cases when no solution exists, e.g. because the departure time or arrival time to the swap-point are too much late and no further ride exists in the same day.
+;
+;Item 3: POINTS  28. Refer to the two choices with the CONSTRAINT that the travel should occur in the same day (i.e. departure and arrival taking place on the same day), compute & print:
+;1.	Name of solution (first/second /NO valid solution exists in the same day);
+;2.	Arrival time to the swap-point (if a valid solution exists);
+;3.	Departure time from the swap-point (if a valid solution exists);
+;4.	Arrival time to the office (if a valid solution exists); 
+;5.	The same information of points (2-4) for the other non-chosen but valid solution (if another one exists).
+;Please observe that there could be cases when no solution exists, e.g. because the departure time or arrival time to the swap-point are too much late and no further ride exists in the same day.
+;
+;Item 4: POINTS  32. Refer to the two choices, without any constraint/assumption about the travel occurring in the same day (i.e. departure can be on one day and arrival on the following day), compute & print:
+;1.	Name of solution (first/second);
+;2.	Arrival time to the swap-point;
+;3.	Departure time from the swap-point;
+;4.	Arrival time to the office; if the arrival is on the day after, a “*” should be printed next to the arrival time;
+;5.	The same information of points 2-4 for the other (non-chosen) solution.
+;Please observe that there will be always solutions for both choices A and B, with the arrival either on the same or the following day. Particular care should be taken when managing arrivals on the following day.  
+;
+;Bonus Item: Duration of the full trip. POINTS  +2 if Item 1/2/3 has been solved previously; POINTS  +3 if Item 4 has been solved previously AND it is managed the case of departure & arrival when they are NOT in the same day. 
+;Examples: A_TO_B = 16 minutes     B_TO_TP = 29 minutes;     C_TO_D = 4 minutes      D_TO_TP = 45 minutes
+;
+;Bus	DEPARTURES FROM SELECTED BUS STOP
+;A	8:10	9:00	9:45	10:30	11:30	12:30	13:15	14:00	15:00	16:00	17:00	18:00
+;B (by A)	7:10	8:15	9:05	10:40	11:00	14:00	14:30	15:00	15:30	16:15	16:45	17:15
+;C	8:30	9:40	10:50	12:00	13:10	14:20	15:30	16:40	17:50	19:00	20:10	21:20
+;D (by C)	7:00	8:40	9:40	10:40	11:40	12:30	13:30	15:00	16:30	18:30	20:30	21:00
+;
+;•	H_LEAVE = 7:00
+;o	for the first choice, we have A departing at 8:10, arriving at the swap-point with B at 8:10+A_TO_B= 8:10+16 = 8:26. The first available bus B is at 9:05 and therefore the final arrival time is 9:05+B_TO_TP = 9.05+29 = 9:34. Total duration of the trip is 9:34-7:00 = 2 hours and 34 minutes
+;o	for the second choice, we have C departing at 8:30, arriving at the swap-point with D at 8:30+C_TO_D = 8:30+4 = 8:34. The first available bus D is at 8:40 and therefore the final arrival time is 8:40+D_TO_TP = 8.40+45 = 9:25. Total duration of the trip is 9:25-7:00 = 2 h. and 25 m.
+;•	H_LEAVE = 16:55
+;o	for the first choice we have A departing at 17:00, arriving at the swap-point with B at 17:00+A_TO_B = 17:00+16 = 17:16. The first available bus B is at 7:10 the next morning; hence the final arrival time is 7:10+B_TO_TP=7:10+29 = 7:39 one day after. Total duration is 7:39+24.00-16:55=14 h. and 44 m. 
+;o	for the second choice, we have C departing at 17:50, arriving at swap-point with D at 17:50+C_TO_D = 17:50+4 = 17:54. The first available bus D is at 18:30 and therefore final arrival time is 18:30+D_TO_TP = 18.30+45 = 19:15. Total duration of the trip is 19:15-16:55 = 2 h. and 20 m. 
+;•	H_LEAVE = 21:30
+;o	for the first choice, we have A departing at 8:10 the next morning, arriving at the swap-point with B at 8:10+A_TO_B = 8:10+16 = 8:26. The first available bus B is at 9:05 and therefore final arrival time is 9:05+B_TO_TP = 9.05+29 = 9:34. Total duration is 9:34+24.00-21:30 = 12 h. and 4 m. 
+;o	for the second choice, we have C departing at 8:30 the next morning, arriving at the swap-point with D at 8:30+C_TO_D = 8:30+4 = 8:34. The first available bus D is at 8:40 and therefore final arrival time is 8:40+ D_TO_TP = 8.40+45 = 9:25. Total duration is 9:25+24.00-21:30=11 h. and 55 m. 
+;
+;HINTS (observe that)
+;•	The format of the time variables allows direct comparisons (lexicographic order): no need to convert
+;•	For each bus departure time, the arrival time is deterministic, as well as the duration of the bus time. A possible solution could be to pre-compute for each bus departure, the final arrival time and then… (As another matter of example, you could think about booking a flight with 2 hops…)
+;
+;IMPORTANT NOTES AND REQUIREMENTS (SHARP)
+;•	It is not required to provide the/an optimal solution, but a working and clear one using all information provided.
+;•	It is required to write at class time a short & clear explanation of the algorithm and significant instruction comments.
+;•	Input-output is not necessary in class-developed solution, but its implementation is mandatory for the oral exam.
+;•	Minimum score to “pass” this part is 15 (to be averaged with second part and to yield a value at least 18)
+;•	To avoid misunderstandings, please consider that, as in the previous calls of the last 5 years (at least) the final score reflects the overall evaluation of the code, i.e., fatal errors, such as division by zero (etc) make it impossible to reach 30 or larger scores. Specifically, at oral exam, students will request the evaluation of some or all the parts that they have solved; prior proceeding to the correction, the points of the parts to be corrected will be added up and bounded to max 35. The final score, after the correction of students’ requested items, will be “cut off” to 32.
+;
+;REQUIREMENTS ON THE I/O PART TO BE DONE AT HOME
+;•	The databases (if any) have to be defined and initialized inside the code; in this case, all data related to the starting time H_LEAVE have to be input from the keyboard (i.e. NOT stored in the array/variables)
+;•	All inputs and outputs should be in readable ASCII form (no binary is permitted).
+;
+;*********************************************************************************************************************************       
+;
+;How to get this file running emu8086:
+;  Simply load this file and then click Emulate, many windows will open with the possibility of debugging and checking everything
+;How to get this file running with MASM:
+;  Download MASM and DOSBox at the link: https://mega.nz/#!4exx2KZI!gnlI4rPvYBhElE-y_k-p9GtBfw550U82ZFB-lHqZJho
+;  Install DOSBox and move the directory named "8086" in C: folder. Copy in 8086 folder this executable
+;  Once in DOSBox, type first " mount c: c:\8086", then "c: ", next "ml filename.asm" and finally "filename.exe"
+
 N_A equ 12     ; N1
 N_B equ 12     ; N2
 N_C equ 12     ; N3
